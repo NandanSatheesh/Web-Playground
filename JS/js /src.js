eb5a18f06edc24c383e0041d381c5b599f832469
Nandan Satheesh <nandan.199776@gmail.com>
2019-01-30 21:27:07 +0530

    JS F**K

--- JS/js /src.js
+++ JS/js /src.js
@@ -1,5 +1,17 @@
 alert("Fuck You");
 
-var name = prompt("Sup? Your Name?");
+var firstname = prompt("Sup? Your First Name?");
 
 alert("Fuck you "+name);
+
+var lastname = prompt("Sup? Your Last Name?");
+
+alert("Fuck you "+name);
+
+var age = prompt("Sup? Your Age?");
+
+alert("You are "+age+" fucking years old");
+
+alert("Your fucking full name is "+firstname+" "+lastname);
+
+alert(age);
