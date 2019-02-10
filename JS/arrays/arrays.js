bae3d20228c68bef4278681253e06b778fbba37b
Nandan Satheesh <nandan.199776@gmail.com>
2019-02-10 14:42:42 +0530

    Arrays

---
+++ JS/arrays/arrays.js
@@ -0,0 +1,12 @@
+
+
+var numbers = ['One','Two','Three']
+numbers[4] = 'Five' ;
+for(var i = 0 ; i < numbers.length ; i++){
+
+
+    console.log(numbers[i])
+    
+    document.write(numbers[i]+'<br>');
+
+}
