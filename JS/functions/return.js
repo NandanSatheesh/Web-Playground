9a6283cf2a9845d97e29e5e479f89ffce68fc82f
Nandan Satheesh <nandan.199776@gmail.com>
2019-02-10 11:59:47 +0530

    Functions in JS

--- JS/functions/return.js
+++ JS/functions/return.js
@@ -1,3 +1,49 @@
+function kebabToSnake(str){
+
+    while(str.indexOf('-') != -1){
+
+        str = str.replace('-','_');
+
+    }
+    
+
+    return str ;
+
+}
+
+
+var str = kebabToSnake(prompt('enter the str in kebab case'));
+console.log(str);
+
+
+function factorial(n){
+    if(n==1){
+        return 1 ;
+    }
+
+    return n * factorial(n -1);
+
+}
+
+var s = factorial(prompt('enter a number for factorial'));
+console.log(s);
+
+function isEven(){
+    var s = prompt('enter a number ');
+    if( s % 2 == 0){
+        return true ;
+    }
+    else{
+        return false ;
+    }
+
+}
+
+var even = isEven();
+console.log(even);
+
+
+
 
 
 function doSomething(){
@@ -9,6 +55,16 @@ function doSomething(){
 
 
 doSomething();
-
-
 doSomething();
+
+
+function square(num){
+
+    console.log(num*num);
+
+
+    return alert('the square of the number is ' + ( num * num ) );
+}
+
+
+square(prompt('enter some number nigga'));
