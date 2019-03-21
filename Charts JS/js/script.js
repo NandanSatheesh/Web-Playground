277b946b21742f113da3df6086635387d9f4fe41
Nandan Satheesh <nandan.199776@gmail.com>
2019-03-22 00:04:13 +0530

    Added more Charts

--- Charts JS/js/script.js
+++ Charts JS/js/script.js
@@ -1,18 +1,30 @@
 console.log('script loaded');
 
+function getRandomData(n){
+
+    n = parseInt(n) ;
+    var arr = [];
+    for (var i=0, t=40; i<t; i++) {
+        arr.push(Math.round(Math.random() * n ))
+    }
+
+    console.log(arr);
+    return arr ;
+
+}
 
 var ctx = document.getElementById('myChart1').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'line',
+    type: 'bar',
 
     // The data for our dataset
     data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
             label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: '#90caf9',
+            borderColor: '#90caf9',
             data: [0, 10, 5, 2, 20, 30, 45]
         }]
     },
@@ -51,9 +63,9 @@ var chartData = {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
             label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            backgroundColor: '#90caf9',
+            borderColor: '#90caf9',
+            data: getRandomData(12)
         }]
     },
 
@@ -63,3 +75,22 @@ var chartData = {
 
 var chart = new Chart(ctx , chartData);
 
+var ctx = document.getElementById('myChart4').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'horizontalBar',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: '#90caf9',
+            borderColor: '#90caf9',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
